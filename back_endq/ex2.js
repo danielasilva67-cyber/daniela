e023192fd1fc6b75f6da9ba3c375ea233bffab83
danielasilva67-cyber <daniela.silva67@aluno.educa.go.gov.br>
2026-02-10 10:51:08 -0300

    Update ex2.js

--- back_endq/ex2.js
+++ back_endq/ex2.js
@@ -1,26 +1,29 @@
 **1.** O que é o **Node.js**?
 Explique sua finalidade e por que ele é considerado um ambiente de execução e não uma linguagem de programação.
+O Node.js é um ambiente de execução JavaScript (runtime) de código aberto e multiplataforma, construído sobre o motor V8 do Google Chrome. Ele permite que os desenvolvedores executem códigos JavaScript fora do navegador web, ou seja, diretamente no computador ou servidor. 
+Alura
+Alura
+ +3
 
----
 
 **2.** Qual a diferença entre **Node.js** e **JavaScript executado no navegador**?
 Cite pelo menos **duas diferenças**.
 
----
+Embora ambos utilizem a linguagem JavaScript, Node.js e o JavaScript no navegador (browser) funcionam como ambientes de execução (runtimes) distintos, cada um com seus próprios objetivos e capacidades. 
 
 **3.** O que é o **V8 Engine** e qual sua importância para o funcionamento do Node.js?
+é um mecanismo (engine) de JavaScript de alto desempenho, de código aberto, desenvolvido pelo Google em C++
 
----
 
 **4.** Explique o conceito de **I/O não bloqueante** no Node.js.
 Por que isso melhora o desempenho de aplicações?
+permite ao ambiente de execução lidar com múltiplas operações simultaneamente, sem travar a execução do código principal enquanto espera por tarefas demoradas, como ler arquivos ou consultar bancos de dados. 
 
----
 
 **5.** O que é o **Event Loop**?
 Descreva, de forma resumida, como ele funciona.
 
----
+---O código é lido de cima para baixo. Tarefas síncronas são colocadas na Call Stack e executadas imediatamente.
 
 **6.** O que são **módulos** no Node.js?
 Explique a diferença entre:
@@ -29,22 +32,19 @@ Explique a diferença entre:
 * Módulos externos
 * Módulos criados pelo desenvolvedor
 
----
+arquivos JavaScript reutilizáveis que encapsulam código, funcionalidades e escopos próprios, permitindo organizar, estruturar e dividir aplicações grandes em partes menores e mais fáceis de gerenciar
 
 **7.** Para que serve o arquivo **package.json** em um projeto Node.js?
 Cite pelo menos **três informações** que ele pode conter.
 
----
+arquivos JavaScript reutilizáveis que encapsulam código, funcionalidades e escopos próprios, permitindo organizar, estruturar e dividir aplicações grandes em partes menores e mais fáceis de gerenciar.
 
 **8.** O que é o **npm**?
 Explique sua função no desenvolvimento de aplicações Node.js.
-
----
+é o gerenciador de pacotes padrão para o ambiente de execução Node.js e o maior repositório de software do mundo.
 
 **9.** O que é uma **API REST** e como o Node.js pode ser usado para desenvolvê-la?
-
----
+é um conjunto de regras e diretrizes arquiteturais que permite que sistemas computacionais troquem informações pela internet de forma segura, flexível e eficiente. 
 
 **10.** Cite **duas vantagens** e **duas desvantagens** do uso do Node.js em aplicações web.
-
----
+é um conjunto de regras e diretrizes arquiteturais que permite que sistemas computacionais troquem informações pela internet de forma segura, flexível e eficiente.
